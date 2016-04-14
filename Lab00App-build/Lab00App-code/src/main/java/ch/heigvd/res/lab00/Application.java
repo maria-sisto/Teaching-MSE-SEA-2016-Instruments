5bd68566b3274d1cd397f7cb03966bcf63fbe9ff
Maria Sisto <maria.sisto@edu.hefr.ch>
2016-04-14 16:37:44 +0200

    Fix add in Application.java

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Application.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Application.java
@@ -5,31 +5,31 @@ package ch.heigvd.res.lab00;
  * cycle. All methods used in the JUnit test are defined, so the test class will
  * compile. There is a bug in the add method, so one of the tests is expected
  * to fail.
- * 
+ *
  * If you look at the class named ch.heigvd.res.lab01.ApplicationTest, you will
  * find an executable specification for this class. The test methods specify
  * the expected behavior for this class.
- * 
+ *
  * @author Olivier Liechti
  */
 public class Application {
 
-  private String message;
+    private String message;
 
-  public Application() {
-    this("HEIG-VD rocks!");
-  }
+    public Application() {
+        this("HEIG-VD rocks!");
+    }
 
-  public Application(String message) {
-    this.message = message;
-  }
+    public Application(String message) {
+        this.message = message;
+    }
 
-  public String getMessage() {
-    return message;
-  }
+    public String getMessage() {
+        return message;
+    }
 
-  public int add(int a, int b) {
-    return a * b;
-  }
+    public int add(int a, int b) {
+        return a + b;
+    }
 
 }
